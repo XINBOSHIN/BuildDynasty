27708ef43c126646012364a1dc123e95cc92cfe8
XINBOSHIN <danilnovichek@vk.com>
2024-11-25 16:54:24 +0300

    Рефакторинг

--- src/main/java/com/xinboshin/builddynasty/BuildDynasty.java
+++ src/main/java/com/xinboshin/builddynasty/BuildDynasty.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.xinboshin.builddynasty;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -10,8 +10,8 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+    public static final String MODID = "build_dynasty";
+    public static final String NAME = "Build Dynasty";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
