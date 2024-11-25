dc83aa23e231b825ad0fcff7bc891b2d1d8b618e
XINBOSHIN <danilnovichek@vk.com>
2024-11-25 17:27:15 +0300

    Update MixinChatTabCompleter.java

--- src/launch/java/baritone/launch/mixins/MixinBlockStateContainer.java
+++ src/launch/java/baritone/launch/mixins/MixinBlockStateContainer.java
@@ -12,10 +12,10 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with Baritone.  If not, see <https://www.gnu.org/licenses/>.
+ * along with builddynasty.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package baritone.launch.mixins;
+package builddynasty.launch.mixins;
 
 import baritone.utils.accessor.IBitArray;
 import baritone.utils.accessor.IBlockStateContainer;
