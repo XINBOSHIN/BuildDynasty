4a4b015aa307de5925ffc8d9a5e3025eb4cdc493
XINBOSHIN <danilnovichek@vk.com>
2024-11-25 16:57:17 +0300

    Обновление build.gradle

--- src/main/java/com/xinboshin/builddynasty/BuildDynasty.java
+++ src/main/java/com/xinboshin/builddynasty/BuildDynasty.java
@@ -7,10 +7,10 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = BuildDynasty.MODID, name = BuildDynasty.NAME, version = BuildDynasty.VERSION)
+public class BuildDynasty
 {
-    public static final String MODID = "build_dynasty";
+    public static final String MODID = "builddynasty";
     public static final String NAME = "Build Dynasty";
     public static final String VERSION = "1.0";
 
